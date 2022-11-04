2e84aab9772e7612d47fb412c009e7651d2d0be6
Jeong Jae Ha <jaeha2324@gmail.com>
2022-11-04 09:57:18 +0900

    inital setting

---
+++ src/constants/responseMessage.js
@@ -0,0 +1,30 @@
+module.exports = {
+  NULL_VALUE: "필요한 값이 없습니다",
+  OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+  NO_AUTHENTICATED: "접근 권한이 없습니다",
+  DB_ERROR: "데이터베이스 오류",
+  INTERNAL_SERVER_ERROR: "서버 내 오류",
+
+  //쿠폰
+  CREATE_TYPE_SUCCESS: "쿠폰유형을 생성하였습니다",
+  ALREADY_EXIST_TYPE: "이미 존재하는 타입입니다.",
+  ALREADY_EXIST_DISCOUNT: "이미 존재하는 할인입니다.",
+  DELETE_TYPE_SUCCESS: "쿠폰유형을 삭제하였습니다",
+  NOT_EXIST_TYPE: "존재하지않는 타입입니다.",
+  EMPTY_TYPE: "타입을 입력해주세요.",
+  EDIT_TYPE_SUCCESS: "타입을 수정하였습니다.",
+  SUCCESS: "성공하였습니다.",
+
+  //쿠폰다운로드
+  COUPON_DOWNLOAD_SUCCESS : "쿠폰이 다운로드 되었습니다.",
+
+  //할인
+  CREATE_DISCOUNT_SUCCESS: "할인유형이 생성되었습니다.",
+  NOT_EXIST_TYPE: "할인타입이 존재하지않습니다.",
+  EDIT_DISCOUNT_SUCCESS: "할인유형이 수정되었습니다.",
+  NOT_EXIST_DISCOUNT: "할인유형이 존재하지않습니다.",
+  OVER_PRICE: "할인은 원화기준 10,000원 달러기준 10달러 미만으로 가능합니다.",
+  DISCOUNT_ALREADY_EXIST: "동일한 할인유형이 존재합니다.",
+  EMPTY: "빈칸없이 입력해주세요.",
+  SUCCESS: "성공하였습니다."
+};
---
+++ src/models/datasource.js
@@ -0,0 +1,17 @@
+const { DataSource } = require("typeorm");
+const posts = require("../entities/posts");
+
+const AppDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  TIME_ZONE: process.env.TIME_ZONE,
+  entities: [posts],
+  synchronize: true,
+  logging: false,
+});
+
+module.exports = { AppDataSource };
